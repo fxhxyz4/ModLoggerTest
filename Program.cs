52e438e45c5341a87b92a02c068b87c3dcb34cd2
fxhxyz <fxhxyz7@proton.me>
2025-02-23 22:38:36 +0000

    _

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ModLogger;
+
+Console.WriteLine("Hello, World!");
+
+// Error message
+ModLog.Error("error");
+
+// Info message
+ModLog.Info("info");
+
+// Log message
+ModLog.Log("log");
+
+// Warn message
+ModLog.Warn("warn");
+
+// Debug message
+ModLog.Debug("debug");
